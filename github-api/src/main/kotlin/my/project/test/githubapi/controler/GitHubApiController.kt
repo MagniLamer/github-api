04f720d17be7c3da9f05c0799385fba8ed6e6ea3
Andrii Voitkevych <avoitkevych@casgate.io>
2023-08-27 21:59:13 +0200

    Changed the mapping of the controller

--- github-api/src/main/kotlin/my/project/test/githubapi/controler/GitHubApiController.kt
+++ github-api/src/main/kotlin/my/project/test/githubapi/controler/GitHubApiController.kt
@@ -3,7 +3,7 @@ package my.project.test.githubapi.controler
 import my.project.test.githubapi.service.GitHubReposService
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -20,15 +20,15 @@ class GitHubApiController(
 ) {
     private val logger = Logger.getLogger(GitHubReposService::class.java.name)
 
-    @GetMapping(path = ["/repo"], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(path = ["/repo"], consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun getUserRepos(
         @RequestBody requestBody: String
     ): ResponseEntity<String> {
-        logger.log(Level.INFO,"\nReceived a request with body -\n$requestBody")
+        logger.log(Level.INFO, "\nReceived a request with body -\n$requestBody")
         return reposService.processRequest(requestBody)
     }
 
-    @GetMapping(value = ["/repo"], consumes = [MediaType.APPLICATION_XML_VALUE])
+    @PostMapping(value = ["/repo"], consumes = [MediaType.APPLICATION_XML_VALUE])
     fun handleInvalidContentType(): ResponseEntity<String> {
         return reposService.handleInvalidContentType()
     }
